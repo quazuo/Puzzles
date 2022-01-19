73307c55b67fa08e1ffca0b23ccb48441e509f56
Filip Głębocki <fg429202@students.mimuw.edu.pl>
2022-01-19 23:00:00 +0100

    idk what changed lmao

--- puzzle.js
+++ puzzle.js
@@ -1,56 +1,31 @@
-const UIEngine = (function () {
+"use strict";
+
+const UIEngine = (function() {
     let areStatsVisible = true;
 
     return {
         uploadImage() {
-            this.hideResultsPopup();
             document.getElementById("fileInput").click();
         },
 
-        showChooseDiff() {
+        showSelectDiff() {
             if (!puzzleEngine.isImageLoaded()) return;
-
-            this.hideResultsPopup();
             let popup = $(`#diff-select`);
             popup.css("pointer-events", "all");
             popup.animate({opacity: 1}, 200);
         },
 
-        hideChooseDiff() {
+        hideSelectDiff() {
             let popup = $(`#diff-select`);
             popup.css("pointer-events", "none");
             popup.animate({opacity: 0}, 200);
-        },
-
-        showResultsPopup() {
-            let popup = $(`#results-popup`);
-            party.confetti(document.getElementById('results-popup'), {
-                count: party.variation.range(50, 70),
-                size: party.variation.range(1.0, 2.0),
-            });
-            popup.css("pointer-events", "all");
-            popup.animate({opacity: 1}, 200);
-        },
-
-        hideResultsPopup() {
-            let popup = $(`#results-popup`);
-            popup.css("pointer-events", "none");
-            popup.animate({opacity: 0}, 200);
-        },
-
-        shuffle() {
-            this.hideResultsPopup();
-            puzzleEngine.shuffle();
-            timerEngine.reset();
-            timerEngine.start();
-            statsEngine.reset();
         },
 
         toggleStats() {
             let popup = $(`#stats`);
             popup.animate({opacity: areStatsVisible ? 0 : 1}, 200);
             areStatsVisible = !areStatsVisible;
-        },
+        }
     }
 })();
 
@@ -61,19 +36,19 @@ const soundsEngine = (function () {
         retard: {element: new Audio('sounds/oofsound.wav')},
     };
 
-    const VOLUME = 0.15;
+    const VOLUME = 0.1;
     let soundCounter = 0;
 
     return {
         init() {
             for (let sound of Object.values(sounds)) {
-                if (sound.nodes) continue;
+                if (sound.arr) continue;
 
-                sound.nodes = [];
+                sound.arr = [];
 
                 for (let i = 0; i < 10; i++) {
-                    sound.nodes.push(sound.element.cloneNode());
-                    sound.nodes[i].volume = VOLUME;
+                    sound.arr.push(sound.element.cloneNode());
+                    sound.arr[i].volume = VOLUME;
                 }
             }
         },
@@ -81,7 +56,7 @@ const soundsEngine = (function () {
         play(name) {
             if (!sounds[name]) return;
 
-            sounds[name].nodes[soundCounter].play();
+            sounds[name].arr[soundCounter].play();
 
             soundCounter++;
             if (soundCounter === 10)
@@ -111,15 +86,13 @@ const statsEngine = (function () {
                 return;
 
             stats[stat]++;
-            for (let el of document.getElementsByClassName(stat))
-                el.innerHTML = stats[stat];
+            document.getElementById(stat).innerHTML = stats[stat];
         },
 
         reset() {
             for (let stat in stats) {
                 stats[stat] = 0;
-                for (let el of document.getElementsByClassName(stat))
-                    el.innerHTML = "0";
+                document.getElementById(stat).innerHTML = stats[stat];
             }
         }
     }
@@ -130,23 +103,23 @@ const timerEngine = (function () {
     let timerID = null;
 
     return {
-        getTime: () => time,
+        getTime() {
+            return time;
+        },
 
         tick() {
             time++;
-            let text = "";
             if (time % 60 < 10)
-                text = `${Math.floor(time / 60)}:0${time % 60}`;
+                document.getElementById("timer").childNodes[2].textContent =
+                    `Time: ${Math.floor(time / 60)}:0${time % 60}`;
             else
-                text = `${Math.floor(time / 60)}:${time % 60}`;
-
-            document.getElementById("timer").childNodes[2].textContent = "Time: " + text;
-            document.getElementById("final-time").textContent = text;
+                document.getElementById("timer").childNodes[2].textContent =
+                    `Time: ${Math.floor(time / 60)}:${time % 60}`;
         },
 
         start() {
-            if (!timerID) timerID = setInterval(this.tick, 1000);
-            // might need to bind when code changes, but rn `time` is just a var in the lexical environment
+            if (!timerID) timerID = setInterval(this.tick.bind(this), 1000);
+            // this.tick without the .bind(this) also works, idk why tho
         },
 
         stop() {
@@ -160,20 +133,20 @@ const timerEngine = (function () {
             this.stop();
             time = -1;
             this.tick();
-        },
+        }
     }
 })();
 
 const puzzleEngine = (function () {
     let image = null;
     let id1 = null;
-    let initialSrcs = [];
-    let pieces = [];
-    let correctCounter = 0;
+    let imagePieces = [];
     let selectedDiff = {
         cols: 0,
         rows: 0,
     };
+    let diffs = [];
+    let correctCounter = 0;
 
     const removeAll = () => {
         $('#layer1 p').remove();
@@ -186,10 +159,7 @@ const puzzleEngine = (function () {
         $('#puzzlebg img').remove();
     };
 
-    const cutImageUp = () => {
-        const pieceWidth = image.naturalWidth / selectedDiff.cols;
-        const pieceHeight = image.naturalHeight / selectedDiff.rows;
-
+    const cutImageUp = (pieceWidth, pieceHeight) => {
         for (let y = 0; y < selectedDiff.rows; y++) {
             for (let x = 0; x < selectedDiff.cols; x++) {
                 const canvas = document.createElement('canvas');
@@ -200,18 +170,15 @@ const puzzleEngine = (function () {
                 context.drawImage(image, x * pieceWidth, y * pieceHeight,
                     pieceWidth, pieceHeight, 0, 0, canvas.width, canvas.height);
 
-                initialSrcs.push(canvas.toDataURL());
+                imagePieces.push(canvas.toDataURL());
             }
         }
     };
 
-    const fillContainers = () => {
+    const fillContainers = (pieceWidth, pieceHeight) => {
         const layer1 = $('#layer1');
         const layer2 = $('#layer2');
         const whitebg = $('#whitebg');
-
-        const pieceWidth = image.naturalWidth / selectedDiff.cols;
-        const pieceHeight = image.naturalHeight / selectedDiff.rows;
 
         for (let i = 0; i < selectedDiff.cols * selectedDiff.rows; i++) {
             if (i % selectedDiff.cols === 0 && i !== 0) {
@@ -220,31 +187,51 @@ const puzzleEngine = (function () {
                 whitebg.append('<br>');
             }
 
-            layer1.append(`<img id="${i}" src="${initialSrcs[i]}" onclick="puzzleEngine.swapPuzzles(this.id)" draggable="false" alt=""/>`);
-            pieces.push(document.getElementById(`${i}`));
-            layer2.append(`<img id="${i}_x" src="https://i.ibb.co/1ZhYj3v/block.png" height=${pieceHeight} width=${pieceWidth} draggable="false" style="opacity:0" alt=""/>`);
-            whitebg.append(`<img id="${i}_w" src="https://i.imgur.com/j2HZfhe.png" height=${pieceHeight} width=${pieceWidth} draggable="false" alt=""/>`);
+            layer1.append(`<img id=${i} src=${imagePieces[i]} onclick="puzzleEngine.swapPuzzles(this.id)" draggable="false" alt=""/>`);
+            layer2.append(`<img id=${i}_x src=https://i.ibb.co/1ZhYj3v/block.png height=${pieceHeight} width=${pieceWidth} draggable="false" style="opacity:0" alt=""/>`);
+            whitebg.append(`<img id=${i}_bg src=https://i.imgur.com/j2HZfhe.png height=${pieceHeight} width=${pieceWidth} draggable="false" alt=""/>`);
         }
     };
 
     const swapSources = (id1, id2) => {
-        [ pieces[id1].src, pieces[id2].src ] = [ pieces[id2].src, pieces[id1].src ];
-    };
+        const temp = document.getElementById(id1).src;
+        document.getElementById(id1).src = document.getElementById(id2).src;
+        document.getElementById(id2).src = temp;
+    }
 
     const initiallyCorrectCount = () => {
-        let count = 0;
+        let counter = 0;
 
         for (let i = 0; i < selectedDiff.cols * selectedDiff.rows; i++) {
-            if (pieces[i].src === initialSrcs[i])
-                count++;
+            if (document.getElementById(`${i}`).src === imagePieces[i])
+                counter++;
         }
 
-        return count;
-
-        // could flex by doing
-        // return pieces.reduce((count, piece, i) => piece.src === initialSrcs[i] ? count + 1 : count, 0);
-        // but i aint no flexer
+        return counter;
     }
+
+    const alertEndResults = () => {
+        const time = timerEngine.getTime();
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        const [moves, misses, doubles, retard] = statsEngine.pull();
+
+        if (seconds < 10) {
+            alert(`Well done!
+Time: ${minutes}:0${seconds}
+Moves: ${moves}
+Misses: ${misses}
+Doubles: ${doubles}
+Retard: ${retard}`);
+        } else {
+            alert(`Well done!
+Time: ${minutes}:${seconds}
+Moves: ${moves}
+Misses: ${misses}
+Doubles: ${doubles}
+Retard: ${retard}`);
+        }
+    };
 
     return {
         initImage() {
@@ -254,7 +241,9 @@ const puzzleEngine = (function () {
             image.setAttribute('crossOrigin', 'anonymous');
         },
 
-        isImageLoaded: () => image.src !== "",
+        isImageLoaded() {
+            return diffs.length;
+        },
 
         loadImage: (file) => new Promise((resolve, reject) => {
             image.onload = () => {
@@ -266,22 +255,22 @@ const puzzleEngine = (function () {
         }),
 
         selectDiff: (clicked_id) => {
-            const diffText = document.getElementById(clicked_id).textContent;
-            const [text1, text2] = diffText.split("x");
+            let diffText = document.getElementById(clicked_id).textContent;
+            let [text1, text2] = diffText.split("x");
             selectedDiff = {
                 cols: parseInt(text1),
                 rows: parseInt(text2),
             };
-            UIEngine.hideChooseDiff();
+            UIEngine.hideSelectDiff();
             puzzleEngine.generatePuzzles();
         },
 
         processDiffs() {
-            let diffsCount = 0;
+            diffs = [];
             const diffSelect = $('#diff-select');
             diffSelect.empty();
 
-            const {naturalWidth: w, naturalHeight: h} = image;
+            const { naturalWidth: w, naturalHeight: h } = image;
 
             for (let i = 25; i < w; i++) {
                 if (w % i !== 0)
@@ -290,19 +279,17 @@ const puzzleEngine = (function () {
                 for (let j = -1 * Math.floor(i * 0.11); j < i * 0.11; j++) {
                     if (h % (i - j) === 0) {
                         const pieceWidth = i, pieceHeight = i - j;
+                        diffs.push([pieceWidth, pieceHeight]);
 
                         let desc = `${w / pieceWidth}x${h / pieceHeight} (${pieceWidth}x${pieceHeight}px)`;
-                        diffSelect.append(`<a class="diff" id="diff_${diffsCount}" onclick="puzzleEngine.selectDiff(this.id)">${desc}</a>`);
-                        diffsCount++;
+                        diffSelect.append(`<a class="diff" id="diff_${diffs.length - 1}" onclick="puzzleEngine.selectDiff(this.id)">${desc}</a>`);
                     }
                 }
             }
 
-            if (diffsCount === 0) {
-                image.src = "";
+            if (diffs.length === 0)
                 alert('No possible square or almost square difficulties possible. ' +
                     'Please choose a picture with a common aspect ratio.');
-            }
         },
 
         generatePuzzles() {
@@ -311,15 +298,19 @@ const puzzleEngine = (function () {
                 return;
             }
 
-            const containers = document.getElementsByClassName('container');
-            for (let cont of containers)
-                cont.style.width = `${image.naturalWidth}`;
+            const containersArray = document.getElementsByClassName('container');
+            const containersCount = containersArray.length;
+            for (let i = 0; i < containersCount; i++)
+                containersArray[i].style.width = `${image.naturalWidth}`;
 
-            initialSrcs = pieces = [];
+            imagePieces = [];
+
+            let pieceWidth = image.naturalWidth / selectedDiff.cols;
+            let pieceHeight = image.naturalHeight / selectedDiff.rows;
 
             removeAll();
-            cutImageUp();
-            fillContainers();
+            cutImageUp(pieceWidth, pieceHeight);
+            fillContainers(pieceWidth, pieceHeight);
         },
 
         shuffle() {
@@ -333,10 +324,13 @@ const puzzleEngine = (function () {
 
             correctCounter = initiallyCorrectCount();
             // maybe implement a re-shuffle if too many are correct initially? idk
+            timerEngine.reset();
+            timerEngine.start();
+            statsEngine.reset();
         },
 
         swapPuzzles: (id2) => {
-            if (pieces[id2].src === initialSrcs[id2]) {
+            if (document.getElementById(id2).src === imagePieces[id2]) {
                 soundsEngine.play("retard");
                 statsEngine.update("retard");
                 document.getElementById(`${id2}_x`).style.opacity = '1';
@@ -346,8 +340,8 @@ const puzzleEngine = (function () {
 
             soundsEngine.play("hit");
 
-            if (id1 === null) {
-                pieces[id2].style.opacity = '0.7';
+            if (!id1) {
+                document.getElementById(id2).style.opacity = '0.7';
                 id1 = id2;
                 return;
 
@@ -355,15 +349,15 @@ const puzzleEngine = (function () {
                 statsEngine.update("moves");
                 swapSources(id1, id2);
 
-                const src1 = pieces[id1].src;
-                const src2 = pieces[id2].src;
+                const src1 = document.getElementById(id1).src;
+                const src2 = document.getElementById(id2).src;
 
-                if (src1 === initialSrcs[id1] && src2 === initialSrcs[id2]) {
+                if (src1 === imagePieces[id1] && src2 === imagePieces[id2]) {
                     correctCounter += 2;
                     soundsEngine.playDouble();
                     statsEngine.update("doubles");
 
-                } else if (src1 === initialSrcs[id1] || src2 === initialSrcs[id2]) {
+                } else if (src1 === imagePieces[id1] || src2 === imagePieces[id2]) {
                     correctCounter++;
                     soundsEngine.play("good");
 
@@ -372,12 +366,12 @@ const puzzleEngine = (function () {
                 }
             }
 
-            pieces[id1].style.opacity = '1';
+            document.getElementById(id1).style.opacity = '1';
             id1 = null;
 
             if (correctCounter === selectedDiff.cols * selectedDiff.rows) { // puzzle is solved
                 timerEngine.stop();
-                setTimeout(UIEngine.showResultsPopup, 500);
+                setTimeout(alertEndResults, 500);
             }
         },
     }
